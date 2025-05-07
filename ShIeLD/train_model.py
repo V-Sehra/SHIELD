516443740c115a4e3a3e47a6d25918df8509dd6f
Vivek <vivek.sehra@uni-tuebingen.de>
2025-05-07 11:53:23 +0200

    introduced lock to ensure not overlapping saving

--- ShIeLD/train_model.py
+++ ShIeLD/train_model.py
@@ -10,6 +10,8 @@ import pickle
 from pathlib import Path
 import torch
 import pandas as pd
+from filelock import FileLock
+import time
 from tqdm import tqdm
 from torch_geometric.loader import DataListLoader
 
@@ -45,6 +47,10 @@ def main():
     split_number = int(args.split_number)
 
     training_results_csv, csv_file_path = train_utils.get_train_results_csv(requirement_dict=requirements)
+    # Create a lock file to prevent concurrent writes
+    lock_file_path = csv_file_path.with_suffix(csv_file_path.suffix + ".lock")
+    lock = FileLock(lock_file_path, timeout=60)
+
     meta_columns = ['anker_value', 'radius_distance', 'fussy_limit',
                     'dp', 'comment', 'comment_norm', 'model_no','split_number']
 
@@ -136,8 +142,10 @@ def main():
                             print('val_bal_acc', 'val_f1_score')
                             print(val_bal_acc,val_f1_score)
 
-                            training_results_csv = pd.concat([model_csv, training_results_csv], ignore_index=True)
-                            training_results_csv.to_csv(csv_file_path, index=False)
+                            with lock:
+                                training_results_csv = pd.concat([model_csv, training_results_csv], ignore_index=True)
+                                training_results_csv.to_csv(csv_file_path, index=False)
+
                             # reload to avoid overwriting issues when running the code in paralell
                             training_results_csv = pd.read_csv(csv_file_path)
 
