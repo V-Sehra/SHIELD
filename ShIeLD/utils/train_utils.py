5c14b45ad403542c55736738ca871a24500e271c
Vivek <vivek.sehra@uni-tuebingen.de>
2025-10-21 09:20:08 +0200

    changed the loss function to NLLLoss as the model now parses the probabilites directly

--- ShIeLD/utils/train_utils.py
+++ ShIeLD/utils/train_utils.py
@@ -36,7 +36,6 @@ def early_stopping(loss_epoch: List, patience: Optional[int] = 15) -> bool:
         raise ValueError("Patience should be greater than 1.")
 
     if len(loss_epoch) >= patience:
-
         if (loss_epoch[-2] - loss_epoch[-1]) < 0.001:
             return True
         else:
@@ -58,39 +57,44 @@ def get_train_results_csv(requirement_dict: dict) -> Tuple[DataFrame, PosixPath]
     """
 
     # Get the path where the training results CSV should be stored
-    path_save_csv_models = requirement_dict['path_training_results']
+    path_save_csv_models = requirement_dict["path_training_results"]
 
     # Ensure the directory exists (creates it if necessary)
     path_save_csv_models.mkdir(parents=True, exist_ok=True)
 
     # Define the expected CSV file path based on the split number
-    csv_file_path = Path(path_save_csv_models / f'validation_split_results_training.csv')
+    csv_file_path = Path(
+        path_save_csv_models / f"validation_split_results_training.csv"
+    )
 
     # If the file exists, load it; otherwise, create an empty DataFrame with predefined columns
     if csv_file_path.exists():
         training_results_csv = pd.read_csv(csv_file_path)
 
     else:
-        training_results_csv = pd.DataFrame(columns=[
-            'anker_value',  # Percentage of anchor cells used
-            'radius_distance',  # Radius defining the neighborhood
-            'fussy_limit',  # Threshold for fuzzy logic application
-            'droupout_rate',  # Dropout rate
-            'comment',  # General comments
-            'comment_norm',  # Normalized comments
-            'model_no',  # Model number
-            'bal_acc_train',  # Balanced accuracy on the training set
-            'train_f1_score',
-            'bal_acc_validation',  # Balanced accuracy on the validation set
-            'val_f1_score',
-            'split_number',  # Validation split number
-        ])
+        training_results_csv = pd.DataFrame(
+            columns=[
+                "anker_value",  # Percentage of anchor cells used
+                "radius_distance",  # Radius defining the neighborhood
+                "fussy_limit",  # Threshold for fuzzy logic application
+                "droupout_rate",  # Dropout rate
+                "comment",  # General comments
+                "comment_norm",  # Normalized comments
+                "model_no",  # Model number
+                "bal_acc_train",  # Balanced accuracy on the training set
+                "train_f1_score",
+                "bal_acc_validation",  # Balanced accuracy on the validation set
+                "val_f1_score",
+                "split_number",  # Validation split number
+            ]
+        )
 
     return training_results_csv, csv_file_path  # Return the DataFram
 
 
-def initiaize_loss(path: str, device: str, tissue_dict: dict,
-                   noise_yLabel: Union[bool, str] = False) -> nn.CrossEntropyLoss:
+def initiaize_loss(
+    path: str, device: str, tissue_dict: dict, noise_yLabel: Union[bool, str] = False
+) -> nn.CrossEntropyLoss:
     """
     This function initializes the loss function as weighted loss.
     It calculates the class weights based on the number of graphs for each tissue type in the training data.
@@ -111,69 +115,84 @@ def initiaize_loss(path: str, device: str, tissue_dict: dict,
 
     if noise_yLabel == False:
         if type(path) == str or type(path) == PosixPath:
-            with open(path, 'rb') as f:
+            with open(path, "rb") as f:
                 all_train_file_names = pickle.load(f)
         elif type(path) == list:
             all_train_file_names = path
 
         class_weights = []
         for origin in tissue_dict.keys():
-            number_tissue_graphs = len([file_name for file_name in all_train_file_names
-                                        if origin in file_name])
+            number_tissue_graphs = len(
+                [file_name for file_name in all_train_file_names if origin in file_name]
+            )
             class_weights.append(1 - (number_tissue_graphs / len(all_train_file_names)))
 
         class_weights = torch.tensor(class_weights, dtype=torch.float32).to(device)
 
     elif noise_yLabel is True:
-        print('initializing loss with noise labels: No Voronoi')
+        print("initializing loss with noise labels: No Voronoi")
 
         class_weights = np.zeros((len(tissue_dict.keys()), 1))
 
         for files in tqdm(os.listdir(Path(path))):
             try:
                 g = torch.load(Path(path) / files)
-                class_weights[g['y']] += 1
+                class_weights[g["y"]] += 1
             except:
-                print('skipping file: ', files, ' due to error in loading or missing label')
+                print(
+                    "skipping file: ",
+                    files,
+                    " due to error in loading or missing label",
+                )
 
         class_weights = 1 - (class_weights.T / sum(class_weights))
-        class_weights = torch.tensor(class_weights.flatten(), dtype=torch.float32).to(device)
+        class_weights = torch.tensor(class_weights.flatten(), dtype=torch.float32).to(
+            device
+        )
 
     else:
-        print('initializing loss with noise labels: ', noise_yLabel)
+        print("initializing loss with noise labels: ", noise_yLabel)
 
-        if noise_yLabel != 'even' and noise_yLabel != 'prob':
-            raise ValueError(f"Invalid noise_yLabel: {noise_yLabel}. Must be 'even' or 'prob'.")
+        if noise_yLabel != "even" and noise_yLabel != "prob":
+            raise ValueError(
+                f"Invalid noise_yLabel: {noise_yLabel}. Must be 'even' or 'prob'."
+            )
 
         class_weights = np.zeros((len(tissue_dict.keys()), 1))
 
         for files in tqdm(os.listdir(Path(path))):
             try:
                 g = torch.load(Path(path) / files)
-                if f'y_noise_{noise_yLabel}' in g:
-                    class_weights[g[f'y_noise_{noise_yLabel}']] += 1
+                if f"y_noise_{noise_yLabel}" in g:
+                    class_weights[g[f"y_noise_{noise_yLabel}"]] += 1
                 else:
-                    class_weights[g['y']] += 1
+                    class_weights[g["y"]] += 1
             except:
-                print('skipping file: ', files, ' due to error in loading or missing label')
+                print(
+                    "skipping file: ",
+                    files,
+                    " due to error in loading or missing label",
+                )
 
         class_weights = 1 - (class_weights.T / sum(class_weights))
-        class_weights = torch.tensor(class_weights.flatten(), dtype=torch.float32).to(device)
+        class_weights = torch.tensor(class_weights.flatten(), dtype=torch.float32).to(
+            device
+        )
 
-        print('done!')
+        print("done!")
 
-    return nn.CrossEntropyLoss(weight=class_weights).to(device)
+    return nn.NLLLoss(weight=class_weights).to(device)
 
 
 def train_loop_shield(
-        optimizer: torch.optim.Optimizer,
-        model: torch.nn.Module,
-        data_loader: DataLoader,
-        loss_fkt: nn.CrossEntropyLoss,
-        attr_bool: bool,
-        device: str,
-        patience: Optional[int] = 5,
-        noise_yLabel: Union[bool, str] = False
+    optimizer: torch.optim.Optimizer,
+    model: torch.nn.Module,
+    data_loader: DataLoader,
+    loss_fkt: nn.CrossEntropyLoss,
+    attr_bool: bool,
+    device: str,
+    patience: Optional[int] = 5,
+    noise_yLabel: Union[bool, str] = False,
 ) -> Tuple[torch.nn.Module, List[float]]:
     """
     Trains the ShIeLD model using the provided optimizer, data loader, and loss function.
@@ -200,11 +219,10 @@ def train_loop_shield(
     # Flag for early stopping condition
     early_stopping_bool = False
 
-    print('Start training')
+    print("Start training")
 
     # Continue training until the early stopping condition is met
     while not early_stopping_bool:
-
         # List to store batch-wise loss values for the current epoch
         loss_batch = []
 
@@ -219,7 +237,7 @@ def train_loop_shield(
                 model=model,
                 device=device,
                 per_patient=False,  # Whether to track patient-level predictions
-                noise_yLabel=noise_yLabel
+                noise_yLabel=noise_yLabel,
             )
 
             # Compute the loss between model output and ground truth labels
